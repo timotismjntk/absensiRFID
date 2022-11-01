62fb450d518f7119db4985ca7e5236a2dcfdd74d
timot24 <timotiussimanjuntak24@gmail.com>
2022-11-01 21:56:52 +0700

    fixing bug

--- src/V3/screens/BLOG/DetailBlog.js
+++ src/V3/screens/BLOG/DetailBlog.js
@@ -106,46 +106,7 @@ export default function DetailBlog({route}) {
               }}
               contentWidth={windowWidth}
               source={{
-                html:
-                  route.params?.konten ||
-                  `Phone 14, iPhone 14 Pro dan iPhone 14 Pro Max mulai dijual hari ini, 16 September 2022, setelah sepekan dibuka preorder. Untuk iPhone 14 Plus sendiri baru akan dilempar ke pasaran awal Oktober.
-                Kendati iPhone 14, iPhone 14 Pro dan iPhone 14 Pro Max belum hadir resmi di Indonesia, kamu bisa membeli ketiga HP tersebut di sejumlah negara yang sudah mulai menjual.
-                
-                Tapi setibanya di Indonesia wajib membayar pajak IMEI di Bea Cukai. Ini supaya iPhone 14 series yang dibeli dapat menerima jaringan milik operator seluler di Tanah Air. Berikut daftar harga ketiga ponsel anyar Apple itu jika berniat membelinya.
-                
-                Harga iPhone 14 dan iPhone 14 Pro di AS
-                
-                Harga iPhone 14 dan iPhone 14 Pro di AS
-                iPhone 14 128GB - USD 829 atau Rp 12,4 juta
-                iPhone 14 256GB - USD 929 atau Rp 13,8 juta
-                iPhone 14 512GB - USD 1.129 atau Rp 16,8 juta
-                
-                iPhone 14 Pro 128GB- USD 999 atau Rp 14,9 juta
-                iPhone 14 Pro 256GB- USD 1.099 atau Rp 16,4 juta
-                iPhone 14 Pro 512GB - USD 1.299 atau Rp 19,3 juta
-                iPhone 14 Pro 1TB - USD 1.499 atau Rp 22,3 juta
-                
-                iPhone 14 Pro Max 128GB- USD 1.099 atau Rp 16,4 juta
-                iPhone 14 Pro Max 256GB- USD 1.199 atau Rp 17,9 juta
-                iPhone 14 Pro Max 512GB - USD 1.399 atau Rp 20,8 juta
-                iPhone 14 Pro Max 1TB - USD 1.599 atau Rp. 23,8 juta
-                
-                Harga iPhone 14 dan iPhone 14 Pro di Singapura
-                
-                Harga iPhone 14 dan iPhone 14 Pro di Singapura
-                iPhone 14 128 GB SGD 1.299 atau Rp 13,8 juta
-                iPhone 14 256 GB SGD 1.469 atau Rp 15,6 juta
-                iPhone 14 512 GB SGD 1.799 atau Rp 19,1 juta
-                
-                iPhone 14 Pro 128 GB SGD 1.649 atau Rp 17,5 juta
-                iPhone 14 Pro 256 GB SGD 1.819 atau Rp 19,3 juta
-                iPhone 14 Pro 512 GB SGD 2.149 atau Rp 22,8 juta
-                iPhone 14 Pro 1TB SGD 2.479 atau Rp 26,3 juta
-                
-                iPhone 14 Pro Max 128 GB SGD 1.799 atau Rp 19,08 juta
-                iPhone 14 Pro Max 256 GB SGD 1.969 atau Rp 20,8 juta
-                iPhone 14 Pro Max 512 GB SGD 2.299 atau Rp 24,3 juta
-                iPhone 14 Pro Max 1TB SGD 2.629 atau Rp 27,9 juta`,
+                html: route.params?.konten || '',
               }}
               renderersProps={renderersProps}
               domVisitors={{
